6f550968b223a66bfb999954953060504403c247
azza14 <42944667+azza14@users.noreply.github.com>
2021-12-15 19:34:21 +0200

    Add project files.

---
+++ TestMultiLangAzza/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestMultiLangAzza.Startup))]
+namespace TestMultiLangAzza
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
